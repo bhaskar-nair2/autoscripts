c78bd7eae1441acd7fb9b29aed103169b3fe662b
bhaskar <b.bhaskar.nair@gmail.com>
2020-04-14 09:22:33 +0530

    Changes to arrowCommands

--- arrowBack.sh
+++ arrowBack.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 cd ~/Documents/arrow-clothing/arrow-backend-master
 git pull
-npm i
-cd services/
-find . -maxdepth 3 -name "package.json" -type f  -exec echo {} \; -exec npm install \;
+find . -maxdepth 4 -path ./node_modules -prune -o -name "package.json" -type f  -exec echo {} \; -exec npm install \;
+cd ../
 docker-compose -f docker-compose.yml up -d
 docker logs -f --tail 10 arrow-backend-master_api_1
--- baseStart.sh
+++ baseStart.sh
@@ -1,4 +1,6 @@
 #/bin/sh
+sudo discord &
+read -p
+sudo google-chrome &
 xdg-open https://localhost:8080 &
-xdg-open https://www.youtube.com &
-discord
+xdg-open https://www.youtube.com
